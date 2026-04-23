9f33a759632b37a4fdd1ef026c791d147fced3f6
OliverPiovarci <oliverpiovarci9@gmail.com>
2026-04-23 15:27:52 +0200

    Samostatný úkol 1

--- sorting.py
+++ sorting.py
@@ -14,9 +14,19 @@ def selection_sort(values):
         zoradeny[i], zoradeny[minimum] = zoradeny[minimum], zoradeny[i]
     return zoradeny
 
+def bubble_sort(seznam):
+    novy = seznam.copy()
+    for i in range(len(novy)):
+        for j in range(0, len(novy)-1-i):
+            if novy[j] > novy[j+1]:
+                novy[j], novy[j+1] = novy[j+1], novy[j]
+    return novy
+
 if __name__ == "__main__":
     values = random_numbers(20)
     print(values)
     test = [5, 1, 4, 2, 8]
     print(selection_sort(random_numbers(20)))
     print(selection_sort(test))
+    print(bubble_sort(test))
+    print(selection_sort(random_numbers(20)))
