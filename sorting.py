e31b6f9b2b48460d957c4a350b62740d633c77cd
OliverPiovarci <oliverpiovarci9@gmail.com>
2026-04-23 14:20:31 +0200

    Samostatný úkol 1

---
+++ sorting.py
@@ -0,0 +1,9 @@
+import random
+
+def random_numbers(count, low=0, high=100):
+    return [random.randint(low, high) for _ in range(count)]
+
+if __name__ == "__main__":
+    values = random_numbers(10)
+    print(values)
+
